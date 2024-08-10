11c717b967e845230783004813e5546266d51859
chamipon <zkucera14@gmail.com>
2024-08-09 22:46:48 -0400

    GET /tasks endpoint

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: __dirname });
 });
 
+app.get("/tasks", (req, res) => {
+	pool.connect()
+		.then(() => {
+			pool.query("SELECT * FROM tasks", (err, result) => {
+				if (err) {
+					console.error("Error executing query", err);
+				} else {
+					res.status(200).send(result.rows);
+					console.log("Query result:", result.rows);
+				}
+			});
+		})
+		.catch((err) => {
+			console.error("Error connecting to PostgreSQL database", err);
+		});
+});
 app.listen(PORT, () => {
 	console.log(`Now listening on port ${PORT}`);
 });
