39b8ceab32541d8b5b9ff2f41f33d1bd903edc3c
chamipon <zkucera14@gmail.com>
2024-08-09 22:46:33 -0400

    Postgres connection, .env added to gitignore

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.API_PORT || 8888;
+const pool = new Pool({
+	user: process.env.POSTGRES_USER || "postgres",
+	password: process.env.POSTGRES_PWD || "admin",
+	host: process.env.POSTGRES_HOST || "localhost",
+	port: process.env.POSTGRES_PORT || "5432",
+	database: process.env.POSTGRES_DB || "mydatabase",
+});
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: __dirname });
 });
-app.get("/tasks", function (req, res) {});
+
 app.listen(PORT, () => {
 	console.log(`Now listening on port ${PORT}`);
 });
